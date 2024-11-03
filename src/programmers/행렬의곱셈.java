888daf42ec3bc16ea4083f0417732859509fd99f
imjeong <imjeong@jeong>
2024-11-04 01:13:32 +0900

    ..

--- src/programmers/행렬의곱셈.java
+++ src/programmers/행렬의곱셈.java
@@ -11,12 +11,14 @@ public class 행렬의곱셈 {
 		    	 arr1 = new int[][] {{1, 4}, {3, 2}, {4, 1}};
 		    	 arr2 = new int[][] {{3,3},{3,3}};
 		    	
-		        int[][] answer = {};
+		        int[][] answer = new int[arr1.length][arr2[0].length];
 		        
-		        for (int i = 0; i < arr1[0].length; i++) {
-		        	for (int j = 0; j < arr2[0].length; j++) {
-						
+		        for (int i = 0; i < answer.length-1; i++) {
+		        	int result = 0;
+		        	for (int j = 0; j < answer[0].length; j++) {
+						 result += arr1[i][j]*arr2[i][j];
 					}
+		        	System.out.println(result);
 				}
 		        
 		        
@@ -25,5 +27,7 @@ public class 행렬의곱셈 {
 		}
 		
 		Solution sol = new Solution();
+		sol.solution(null, null);
+		
 	}
 }
