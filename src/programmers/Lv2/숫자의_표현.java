3a0070135351bdd512bd5f3a7dd12ab9a9578b2b
imun46 <do2816@gmail.com>
2025-02-05 14:43:50 +0900

    feat: Lv2 숫자의 표현 다른 문제풀이(for문 1개 사용)

--- src/programmers/Lv2/숫자의_표현.java
+++ src/programmers/Lv2/숫자의_표현.java
@@ -5,7 +5,7 @@ public class 숫자의_표현 {
         class Solution {
             public int solution(int n) {
                 int answer = 0;
-                for (int i = 1; i <= n; i++) {
+                /*for (int i = 1; i <= n; i++) {
                     int sum = 0;
                     for (int j = i; j <= n; j++) {
                         sum += j;
@@ -18,10 +18,25 @@ public class 숫자의_표현 {
                             break;
                         }
                     }
-                }
+                }*/
 
+                /* for문 하나로 접근 */
+                /*
+                * 어떤 수 n이 k개의 연속된 자연수의 합으로 표현될 수 있는 경우는,
+                * n은 k의 배수이기 때문 => n의 홀수 약수 개와 동일
+                */
+
+                for (int i = 1; i <= n; i+=2) {
+                    if(n%i == 0){
+                        answer++;
+                    }
+                }
                 return answer;
             }
+
+
+
+
         }
         Solution solution = new Solution();
         System.out.println(solution.solution(15));
