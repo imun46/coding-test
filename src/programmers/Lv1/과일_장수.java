c12e63269a39849a4cc2a8db9852460ff48a7ee6
jeong <do2816@gmail.com>
2025-02-06 10:51:38 +0900

    feat: 과일_장수 완료

---
+++ src/programmers/Lv1/과일_장수.java
@@ -0,0 +1,41 @@
+package programmers.Lv1;
+
+import java.util.*;
+
+public class 과일_장수 {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int k, int m, int[] score) {
+                int answer = 0;
+                List<Integer> list = new ArrayList<>();
+
+                for (int i = 0; i < score.length; i++) {
+                    list.add(score[i]);
+                }
+                list.sort(Comparator.reverseOrder());
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.print(list.get(i));
+                }
+                System.out.println();
+                int rowPoint = list.get(list.size() - 1);
+                int count = 1;
+                for (int i = 0; i < list.size(); i++) {
+                    if(i > 0 && i % (m*count-1) == 0){
+                        count++;
+                        if(list.get(i) == rowPoint && rowPoint != k){ // 2
+                            answer += rowPoint * m;
+                            System.out.println("i=" +i +" answer="+answer);
+                        }else{
+                            answer += list.get(i) * m;
+                            System.out.println(count + " i=" +i +" answer="+answer);
+                        }
+                    }
+                }
+
+                return answer;
+            }
+        }
+        Solution solution = new Solution();
+        System.out.println(solution.solution(4,3,new int[]{4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2}));
+    }
+}
