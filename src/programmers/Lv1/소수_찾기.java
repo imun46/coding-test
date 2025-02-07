5ca173666e1d5babe5d22266dcd476bbbd1f5e1f
jeong <do2816@gmail.com>
2025-02-07 10:49:41 +0900

    fix: Math.sqrt 이용하여 시간 줄이기

--- src/programmers/Lv1/소수_찾기.java
+++ src/programmers/Lv1/소수_찾기.java
@@ -5,21 +5,25 @@ public class 소수_찾기 {
         class Solution {
             public int solution(int n) {
                 int answer = 0;
-                int count = 1;
-                boolean check = false;
 
 
-                for (int i = 1; i <= n; i++) {
-                    for (int j = 2; j < count; j++) {
-                        if(count % j == 0) {
-                            check = false;
+                /*
+                    소수를 판별할 때 j는 √i까지만 검사하면 충분
+                    예: i = 100일 때, 2~99까지 검사하는 대신 2~10까지만 검사해도 됨.
+                    이유: n = a * b가 성립할 때, a와 b 중 적어도 하나는 √n 이하이기 때문.
+                 */
+                if(n < 2) return answer;
+                for (int i = 2; i <= n; i++) {
+                    boolean isPrime = true;
+                    for (int j = 2; j <= Math.sqrt(j); j++) {
+                        if (i % j == 0) {
+                            isPrime = false;
                             break;
                         }
-                        check = true;
                     }
-                    answer = check ? answer + 1 : answer;
-                    count ++;
+                    if (isPrime) { answer++;}
                 }
+
                 return answer;
             }
         }
