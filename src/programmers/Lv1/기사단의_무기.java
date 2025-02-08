86955f59911d4a803a54d8b6850bc39fafdb78c4
jeong <do2816@gmail.com>
2025-02-08 12:12:49 +0900

    fix: 시간초과 문제 O(√N) 문제로 해결

--- src/programmers/Lv1/기사단의_무기.java
+++ src/programmers/Lv1/기사단의_무기.java
@@ -6,30 +6,23 @@ public class 기사단의_무기 {
             public int solution(int number, int limit, int power) {
                 int answer = 0;
 
-                //1. 1~number 약수 배열로 구하기
-                //2. 약수가 담긴 배열을 limit과 비교
+                //1. 1~number 약수 구하기 -> 1~number 까지 다 구하는게 아니라 반만 체크해서 구하고
+                //2. 찾은 개수 만큰 또 count++(단, 중복 값 빼주기)
                 //3. limit 넘으면 power 로 계산
 
-                int[] array = new int[number];
-                for (int i = 0; i < array.length; i++) {
+                for (int i = 1; i <= number; i++) {
                     int count = 0;
-                    int n = 1;
-                    while(n <= i+1) {
-                        if((i+1) % n == 0) {
+                    for (int j = 1; j <= Math.sqrt(i); j++) {
+                        if(i % j == 0) {
                             count++;
+                            if( j != i / j ) count++;
                         }
-                        n++;
                     }
-                    array[i] = count;
-                    System.out.println(array[i]);
-                }
-
-
-                for (int i = 0; i < array.length; i++) {
-                    if(array[i] > limit) {
+                    System.out.println(count);
+                    if(count > limit){
                         answer += power;
                     }else{
-                        answer += array[i];
+                        answer += count;
                     }
                 }
 
